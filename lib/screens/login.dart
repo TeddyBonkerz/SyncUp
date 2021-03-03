235517654f4e594c2b0762ce1684928cb9bfbd71
TeddyBonkerz <radzlaif@gmail.com>
2021-03-03 12:36:25 -0500

    Added google fonts to the pubspec and used a google font.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,117 +11,144 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        resizeToAvoidBottomPadding: false,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            //SyncUp Container
-            Container(
-                child: Stack(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          //SyncUp Container
+          Container(
+            child: Stack(
               children: <Widget>[
                 Container(
                   padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                  child: Text('Sync',
-                      style: TextStyle(
-                          fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  child: Text(
+                    'Sync',
+                    style: GoogleFonts.mcLaren(
+                      textStyle: TextStyle(
+                          fontSize: 80.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
                 Container(
-                  padding: EdgeInsets.fromLTRB(193.0, 110.0, 0.0, 0.0),
-                  child: Text('Up',
-                      style: TextStyle(
+                  padding: EdgeInsets.fromLTRB(210.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Up',
+                    style: GoogleFonts.mcLaren(
+                      textStyle: TextStyle(
                           fontSize: 80.0,
                           fontWeight: FontWeight.bold,
-                          color: Colors.blue[900])),
-                )
+                          color: Colors.blue[900]),
+                    ),
+                  ),
+                ),
               ],
-            )),
-            Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    //Email Field
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'EMAIL',
-                          labelStyle: TextStyle(
-                              //fontWeight: FontWeight.bold,
-                              fontFamily: 'Montserrat',
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue[900]))),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                //Email Field
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'EMAIL',
+                    labelStyle: TextStyle(
+                        //fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        color: Colors.grey),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue[900]),
                     ),
-                    SizedBox(height: 20.0),
-                    //Password Field
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'PASSWORD',
-                          labelStyle: TextStyle(
-                              //fontWeight: FontWeight.bold,
-                              fontFamily: 'Montserrat',
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue[900]))),
-                      obscureText: true,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                //Password Field
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'PASSWORD',
+                    labelStyle: TextStyle(
+                        //fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        color: Colors.grey),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue[900]),
                     ),
-                    SizedBox(height: 20.0),
-                    //Forgot Password Link
-                    Container(
-                        alignment: Alignment(1.0, 0.0),
-                        padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                        child: InkWell(
-                            child: Text('Forgot Password',
-                                style: TextStyle(
-                                    color: Colors.blue[900],
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat',
-                                    decoration: TextDecoration.underline)))),
-                    SizedBox(height: 40.0),
-                    //Login Button
-                    Container(
-                        height: 40.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.blueAccent[900],
-                          color: Colors.blue[900],
-                          elevation: 7.0,
-                          child: GestureDetector(
-                              onTap: () {},
-                              child: Center(
-                                child: Text('LOGIN',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'Montserrat')),
-                              )),
-                        )),
-                    SizedBox(height: 25.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'New to SyncUp?',
-                          style: TextStyle(
-                            fontFamily: 'Montserrat',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+                //Forgot Password Link
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: GoogleFonts.mcLaren(
+                        textStyle: TextStyle(
+                            color: Colors.blue[900],
                             fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                //Login Button
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.blueAccent[900],
+                    color: Colors.blue[900],
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: GoogleFonts.mcLaren(
+                            textStyle: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                        SizedBox(width: 5.0),
-                        InkWell(
-                          onTap: () {},
-                          child: Text(
-                            'Register',
-                            style: TextStyle(
-                                color: Colors.blue[900],
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat',
-                                decoration: TextDecoration.underline),
-                          ),
-                        )
-                      ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'New to SyncUp?',
+                      style: GoogleFonts.mcLaren(
+                        textStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 5.0),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Register',
+                        style: GoogleFonts.mcLaren(
+                          textStyle: TextStyle(
+                              color: Colors.blue[900],
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
                     )
                   ],
-                ))
-          ],
-        ));
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
