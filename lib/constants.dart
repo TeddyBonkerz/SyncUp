692c9734a75df59194c16e589e7ee40b221c7be4
TeddyBonkerz <radzlaif@gmail.com>
2021-03-10 18:54:09 -0500

    Switched up th color scheme a little.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF0D47A1);
+const primaryColor = Color(0xFF7f03fc);
 const primaryLightColor = Color(0xFF2196F3);
+const scaffoldBackgroundColor = Color(0xFFd3ced9);
