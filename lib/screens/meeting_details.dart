e674f6d6ebfe1bd1de39947459bebfe54d4af575
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-03-13 20:24:54 -0500

    Added scrollable attendee list

--- lib/screens/meeting_details.dart
+++ lib/screens/meeting_details.dart
@@ -16,40 +16,58 @@ class _MeetingDetails extends State<MeetingDetails> {
       appBar: AppBar(
         title: Text('MeetingTitle'),
         ),
-        body: Column(
-          children: <Widget>[
-            //General Info: Description, time / date, Complete or not
-            ListTile(
-              contentPadding: const EdgeInsets.all(10.0),
-              title: Text('Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description',
-                    textAlign: TextAlign.center,
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Column(
+                  //General Info: Description, time / date, Complete or not
+                children: <Widget>[
+                ListTile(
+                  contentPadding: const EdgeInsets.all(10.0),
+                  title: Text('Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description Meeting Description',
+                        textAlign: TextAlign.center,
+                  ),
+                ),
+                // Time and Date Info
+                ListTile(
+                  contentPadding: const EdgeInsets.all(10.0),
+                  title: Text('Time / Date'),
+                  subtitle: Text('##:## AM/PM @@,@@@@')
+                ),
+                // Location Info
+                ListTile(
+                  contentPadding: const EdgeInsets.all(10.0),
+                  title: Text('Location'),
+                  subtitle: Text('TestLocation')
+                ),
+                // Meeting Check box for completion status
+                CheckboxListTile(
+                  title: const Text('Meeting Completed: '),
+                  value: checkCompleted,
+                  onChanged: (bool value){
+                    setState(() {
+                      checkCompleted = value;
+                    });
+                  }
+                ),
+              ],),
+              ListTile(
+                contentPadding: const EdgeInsets.all(10.0),
+                title: Text('List of Attendence')
               ),
-            ),
-            // Time and Date Info
-            ListTile(
-              contentPadding: const EdgeInsets.all(10.0),
-              title: Text('Time / Date'),
-              subtitle: Text('##:## AM/PM @@,@@@@')
-            ),
-            // Location Info
-            ListTile(
-              contentPadding: const EdgeInsets.all(10.0),
-              title: Text('Location'),
-              subtitle: Text('TestLocation')
-            ),
-            // Meeting Check box for completion status
-            CheckboxListTile(
-              title: const Text('Meeting Completed: '),
-              value: checkCompleted,
-              onChanged: (bool value){
-                setState(() {
-                  checkCompleted = value;
-                });
-              }
-            ),
-            // List of People Coming
-            
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10, // filler till we have actual list length
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text('ContactName'),
+                subtitle: Text('email')
+              );
+            } 
+          ),
+        )
         ],)
+    )
     );
   }
 }
