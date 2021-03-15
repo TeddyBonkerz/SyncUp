4e8591bdf309cc2d351a80ff107600df8a995925
TeddyBonkerz <radzlaif@gmail.com>
2021-03-15 18:02:54 -0400

    updated meet card listview.

--- lib/components/meeting_cards.dart
+++ lib/components/meeting_cards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncup/constants.dart';
 import 'package:syncup/models/attendeeModel.dart';
 import 'package:syncup/models/meetingModel.dart';
 import 'package:syncup/screens/meeting_details.dart';
@@ -45,64 +46,64 @@ class HomePageCards extends StatelessWidget {
 
 //Actual list of cards
   Widget buildMeetingCard(BuildContext context, int index) {
-    double width = MediaQuery.of(context).size.width * 0.7;
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MeetingDetails(),
-          ),
-        );
-      },
-      //Each card on the homepage
+    //double width = MediaQuery.of(context).size.width * 0.7;
+    return SafeArea(
+      top: false,
+      bottom: false,
       child: Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/default_profile.png'),
-                  fit: BoxFit.fill,
-                ),
+        elevation: 5,
+        margin: EdgeInsets.fromLTRB(6, 12, 6, 0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MeetingDetails(),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: width,
-                    child: Text(
-                      meetingList[index].title,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: primaryColor,
+                  child: Text(
+                    meetingList[index].organizer.substring(0, 1),
+                    style: TextStyle(color: Colors.white),
                   ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    width: width,
-                    child: Text(
-                      meetingList[index].description,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey[500],
+                ),
+                Padding(padding: EdgeInsets.only(left: 16)),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        meetingList[index].title,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
+                      Padding(padding: EdgeInsets.only(top: 8)),
+                      Text(
+                        meetingList[index].description,
+                      ),
+                      Padding(padding: EdgeInsets.only(left: 16)),
+                      Padding(padding: EdgeInsets.only(top: 8)),
+                      Divider(
+                        thickness: 2.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
